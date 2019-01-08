6e3ad73329cefad2d2dd960edfc9c3c96b886c86
iqDF <daniel.thekurniadi@gmail.com>
2019-01-08 17:28:19 +0800

    TryDjango-27 implement pure django forms and validation function

--- app/products/forms.py
+++ app/products/forms.py
@@ -2,6 +2,30 @@ from django import forms
 from .models import Product
 
 class ProductModelForm(forms.ModelForm):
+    title = forms.CharField(
+        label='Product Name', 
+        widget=forms.TextInput(
+            attrs={'placeholder': 'Your Input...'}
+            )
+        )
+
+    email = forms.EmailField()
+
+    description = forms.CharField(
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+                'placeholder': 'Your description...',
+                'class': 'new-class-name two',
+                'id': 'text-area-id',
+                'rows': 10,
+                'cols': 60
+            }
+        )
+    )
+
+    price = forms.DecimalField(initial=89.99)
+
     class Meta:
         model = Product
         fields = [
@@ -9,3 +33,41 @@ class ProductModelForm(forms.ModelForm):
             'price',
             'description',
         ]
+
+    # specific-developer-defined cleaning: 
+    # def clean_<whatever var name/form field you declared>
+    def clean_title(self, *args, **kwargs):
+        title = self.cleaned_data.get('title')
+        if not 'DK' in title:
+            raise forms.ValidationError("Invalid Title. Title should contain DK ")
+        if not 'Keren' in title:
+            raise forms.ValidationError("Invalid Title. Title should contain keren ")
+        return title
+
+    def clean_email(self, *args, **args):
+        email = self.cleaned_data.get('email')
+        if not '@' in email:
+            raise forms.ValidationError("Invalid email address.")
+        return title
+
+
+class RawProductForm(forms.Form):
+    title = forms.CharField(
+        label='Product Name', 
+        widget=forms.TextInput(
+            attrs={'placeholder': 'Your Input...'}
+            )
+        )
+    description = forms.CharField(
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+                'placeholder': 'Your description...',
+                'class': 'new-class-name two',
+                'id': 'text-area-id',
+                'rows': 10,
+                'cols': 60
+            }
+        )
+    )
+    price = forms.DecimalField(initial=89.99)
