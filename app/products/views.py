8ce8d132082c36dc275f12b8ad5e463bc89e8874
iqDF <daniel.thekurniadi@gmail.com>
2019-01-08 17:28:54 +0800

    TryDjango - 26 implement pure forms request handler

--- app/products/views.py
+++ app/products/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from .models import Product
-from .forms import ProductModelForm
+from .forms import ProductModelForm, RawProductForm
 
 # Create your views here.
+
+# def product_create_view(request):
+#     if request.method == 'POST':
+#         form = RawProductForm(request.POST)
+#         if form.is_valid():
+#             # Now data is good
+#             print(form.cleaned_data)
+#             Product.objects.create(**form.cleaned_data)
+#         else:
+#             # Invalid form
+#             print(form.errors)
+#     else:
+#         form = RawProductForm()
+#     context = {
+#         'form': form,
+#     }
+#     return render(request, 'products/product_create.html', context)
+
 def product_create_view(request):
     form = ProductModelForm(request.POST or None)
     if form.is_valid():
@@ -12,7 +30,6 @@ def product_create_view(request):
         'form': form
     }
     return render(request, 'products/product_create.html', context)
-
 
 def product_detail_view(request):
     obj = Product.objects.get(id=2)
