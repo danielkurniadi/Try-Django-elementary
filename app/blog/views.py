432a9074ce173edbf21767b851eb009c133b4d0d
iqDF <daniel.thekurniadi@gmail.com>
2019-01-09 12:06:05 +0800

    TryDjango - 38 implement CBV to create new Object

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 from django.views.generic import (
     CreateView,
     DetailView,
@@ -7,8 +8,12 @@ from django.views.generic import (
     DeleteView
 )
 from .models import Article
-
+from .forms import ArticleModelForm
 # Create your views here.
+
+class ArticleCreateView(CreateView):
+    template_name = 'articles/article_create.html' #generic url: <blog>/<model_name>_list.html
+    form_class = ArticleModelForm
 
 class ArticleListView(ListView):
     template_name = 'articles/article_lists.html' #generic url: <blog>/<model_name>_list.html
@@ -18,8 +23,31 @@ class ArticleDetailView(DetailView):
     template_name = 'articles/article_detail.html'
 
     def get_object(self):
-        print(self.kwargs)
-        id_ = self.kwargs.get('id')
+        id_ = self.kwargs.get('pk')
+        print(id_)
         obj = get_object_or_404(Article, id=id_)
-        print(obj)
         return obj
+
+class ArticleUpdateView(UpdateView):
+    template_name = 'articles/article_create.html' #generic url: <blog>/<model_name>_list.html
+    form_class = ArticleModelForm
+    
+    def get_object(self):
+        id_ = self.kwargs.get('pk')
+        print(id_)
+        obj = get_object_or_404(Article, id=id_)
+        return obj
+
+class ArticleDeleteView(DeleteView):
+    template_name = 'articles/article_delete.html' #generic url: <blog>/<model_name>_list.html
+    form_class = ArticleModelForm
+    
+    def get_object(self):
+        id_ = self.kwargs.get('pk')
+        print(id_)
+        obj = get_object_or_404(Article, id=id_)
+        obj.delete
+        return obj
+
+    def get_success_url(self):
+        return reverse('articles:article-list')
